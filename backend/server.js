11d6c8f767f3eaff93934329667ac7c6917da386
ramenwarlord <ivanyeung12345@gmail.com>
2021-10-05 22:59:09 -0400

    local login

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,30 @@ app.get("/users", async (req, res) => {
   res.json(users);
 });
 
+//create user
 app.post("/users", async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const user = { name: req.body.name, password: hashedPassword };
     users.push(user);
     res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+
+//login user
+app.post("/users/login", async (req, res) => {
+  const user = users.find((user) => user.name === req.body.name);
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("success");
+    } else {
+      res.send("wrong password");
+    }
   } catch {
     res.status(500).send();
   }
